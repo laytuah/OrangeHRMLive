cdeaa3542e516bcabac966a0b88fe6ba5424b447
laytuah <olasupoa.o@gmail.com>
2024-06-25 22:12:27 +0100

    login Implemented

--- OrangeHRMLive/PageObjects/LoginPage.cs
+++ OrangeHRMLive/PageObjects/LoginPage.cs
@@ -8,11 +8,19 @@ namespace OrangeHRMLive.PageObjects
         {
             driver = _driver;
         }
-        protected IWebElement UsernameField => driver.FindElement(By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Username')]"));
+        protected IWebElement UsernameText => driver.FindElement(By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Username')]"));
+        protected IWebElement UsernameField => driver.FindElement(By.XPath("//input[@name='username' and @placeholder='Username']"));
+        protected IWebElement PasswordText => driver.FindElement(By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Password')]"));
+        protected IWebElement PasswordField => driver.FindElement(By.XPath("//input[@name='password' and @placeholder='Password']"));
+        protected IWebElement LoginButton => driver.FindElement(By.XPath("//button[contains(@class,'login-button')]"));
+
         public void Login()
         {
-            //var username = driver.FindElement(By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Username')]"));
-            string a = UsernameField.Text;
+            UsernameField.Clear();
+            UsernameField.SendKeys((UsernameText.Text.Split(':'))[1].Trim());
+            PasswordField.Clear();
+            PasswordField.SendKeys((PasswordText.Text.Split(':'))[1].Trim());
+            LoginButton.Click();
         }
     }
 }
