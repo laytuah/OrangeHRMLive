280961b97869b280f401e0132ee02f4562265fa6
laytuah <olasupoa.o@gmail.com>
2024-08-11 13:02:21 +0100

    refactored

--- OrangeHRMLive/PageObjects/BasePage.cs
+++ OrangeHRMLive/PageObjects/BasePage.cs
@@ -14,6 +14,9 @@ namespace OrangeHRMLive.PageObjects
 
         protected IWebElement Mainmenu_item(string itemName) => Driver.FindElement(By.XPath($"//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name' and normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'))=\"{itemName}\"]"));
         protected IWebElement Button_button(string buttonText) => Driver.FindElement(By.XPath($"//button[(@type='button' or @type = 'submit') and normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'))=\"{buttonText}\"]"));
+        protected IWebElement Select_dropdown(string selectText) => Driver.FindElement(By.XPath($"//div[contains(@class,'oxd-select-dropdown')]//span[contains(normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')),\"{selectText}\")]"));
+
+        
 
         public void LoadAUT()
         {
--- OrangeHRMLive/PageObjects/PIMPage.cs
+++ OrangeHRMLive/PageObjects/PIMPage.cs
@@ -9,9 +9,11 @@ namespace OrangeHRMLive.PageObjects
 
         protected IWebElement TextField(string text) => Driver.FindElement(By.XPath($"//input[normalize-space(translate(@placeholder, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'))=\"{text}\"] | //div[label[translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')=\"{text}\"]]/following-sibling::div//input"));
 
+        //div[contains(@class,'oxd-select-dropdown')]//span[contains(.,'Nigerian')]
+
         public void RegisterNewEmployee()
         {
-            Mainmenu_item("PIM").Click();
+            Mainmenu_item("pim").Click();
             Button_button("add").Click();
             TextField("first name").SendKeys(DataGenerator.GenerateRandomString());
             TextField("middle name").SendKeys(DataGenerator.GenerateRandomString());
