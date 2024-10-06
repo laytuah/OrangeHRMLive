96fc1f1c16a528274ab626f990d7de67c01ce9ae
laytuah <olasupoa.o@gmail.com>
2024-10-06 11:22:22 +0100

    refactored

--- OrangeHRMLive/PageObjects/LoginPage.cs
+++ OrangeHRMLive/PageObjects/LoginPage.cs
@@ -8,9 +8,9 @@ namespace OrangeHRMLive.PageObjects
         public LoginPage(IWebDriver driver) : base(driver) { }
 
         PageElement UsernameText => new PageElement(Driver, By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Username')]"));
-        PageElement UsernameField => new PageElement(Driver, By.XPath("//input[@name='username' and @placeholder='Username']"));
+        PageElement UsernameField => new PageElement(Driver, By.XPath("//input[@name='username' and @placeholder='username']"));
         PageElement PasswordText => new PageElement(Driver, By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Password')]"));
-        PageElement PasswordField => new PageElement(Driver, By.XPath("//input[@name='password' and @placeholder='Password']"));
+        PageElement PasswordField => new PageElement(Driver, By.XPath("//input[@name='password' and @placeholder='password']"));
         PageElement LoginButton => new PageElement(Driver, By.XPath("//button[contains(@class,'login-button')]"));
 
         public void Login()
