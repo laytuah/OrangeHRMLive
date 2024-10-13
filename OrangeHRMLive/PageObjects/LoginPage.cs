5136aff9f09b1e614cca6df8abef52b005f5924e
laytuah <olasupoa.o@gmail.com>
2024-10-13 15:42:26 +0100

    refactored

--- OrangeHRMLive/PageObjects/LoginPage.cs
+++ OrangeHRMLive/PageObjects/LoginPage.cs
@@ -6,10 +6,10 @@ namespace OrangeHRMLive.PageObjects
     {
         public LoginPage(IWebDriver driver) : base(driver) { }
 
-        PageElement UsernameText => new PageElement(Driver, By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Username')]"));
-        PageElement UsernameField => new PageElement(Driver, By.XPath("//input[@name='username' and @placeholder='Username']"));
-        PageElement PasswordText => new PageElement(Driver, By.XPath("//p[@class='oxd-text oxd-text--p' and contains(.,'Password')]"));
-        PageElement PasswordField => new PageElement(Driver, By.XPath("//input[@name='password' and @placeholder='Password']"));
+        PageElement UsernameText => new PageElement(Driver, By.XPath("//p[@class='oxd-text oxd-text--p' and contains(normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')),'username')]"));
+        PageElement UsernameField => new PageElement(Driver, By.XPath("//input[@name='username' and normalize-space(translate(@placeholder, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'))='username']"));
+        PageElement PasswordText => new PageElement(Driver, By.XPath("//p[@class='oxd-text oxd-text--p' and contains(normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')),'password')]"));
+        PageElement PasswordField => new PageElement(Driver, By.XPath("//input[@name='password' and normalize-space(translate(@placeholder, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'))='password']"));
         PageElement LoginButton => new PageElement(Driver, By.XPath("//button[contains(@class,'login-button')]"));
 
         public void Login()
